224ca3063424e1a081202cde488e3d271ee823e9
sakhiy <sakhiyfairuz@gmail.com>
2023-08-25 08:55:53 +0700

    added docker compose and postgres db

--- server.ts
+++ server.ts
@@ -1,4 +1,6 @@
 import express from 'express'
+import {Client} from 'pg'
+
 import dotenv from 'dotenv'
 import { sayHello, getAllUser } from './src/controller/api'
 
@@ -7,7 +9,34 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT
 
+const dbConfig = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT),
+};
+  
+async function connectToDatabase() {
+  const client = new Client(dbConfig);
+
+  try {
+    await client.connect();
+    console.log('Connected to the database');
+
+    // Perform database operations here
+
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+  } finally {
+    await client.end();
+    console.log('Disconnected from the database');
+  }
+}
+
 app.get('/', sayHello)
 app.get('/user', getAllUser)
+
+connectToDatabase()
 
 app.listen(port, () => { console.log(`listening to port ${port}`) })
